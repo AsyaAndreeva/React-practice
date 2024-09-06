c93dadd27b2b18e84a0df49cdcbfea014b48f669
AsyaAndreeva <asiara032@gmail.com>
2024-09-06 16:46:50 +0300

    updated Clicker

--- react-components/src/components/Clicker.js
+++ react-components/src/components/Clicker.js
@@ -8,7 +8,22 @@ export const CLicker = ()=>{
         setClicks(oldClicks => oldClicks + 1);
     }
 
+    const dangerClicks = clicks > 20;
+
+    if(clicks > 30){
+        return <h1>Finished Clicks</h1>
+    }
+
     return (
-    <button onClick={clickHandler}>{clicks}</button>
+        <div>
+            <div>
+                {dangerClicks && <h1>Danger Clicks</h1>}
+                {clicks > 10
+                    ? <h2>Medium clicks</h2>
+                    : <h4>Normal clicks</h4>
+                }
+            </div>
+            <button onClick={clickHandler}>{clicks}</button>
+        </div>
 );
 }
