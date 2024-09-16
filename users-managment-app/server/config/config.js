da4b4587083b0893cd0e3567ae162a635520ebb8
AsyaAndreeva <asiara032@gmail.com>
2024-09-16 11:51:03 +0300

    Added more changes

--- users-managment-app/server/config/config.js
+++
@@ -1,8 +0,0 @@
-const { PORT, USER, PASSWORD, DB_NAME } = process.env;
-
-const productionConnection = `mongodb+srv://${USER}:${PASSWORD}@db.exhqa.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
-module.exports = {
-  port: PORT || 3005,
-  dbConnection: `${productionConnection}`,
-};
